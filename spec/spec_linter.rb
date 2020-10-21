3d678a99a4b142b2c62f516d7db0641c44b3725c
edxco <eduardo.baeza@yahoo.com>
2020-10-21 04:12:33 -0500

    Fix bad.rb rubocop errors

--- spec/spec_linter.rb
+++ spec/spec_linter.rb
@@ -22,11 +22,4 @@ describe LintFile do
       expect(file_open.lines.count).to eql(File.open(original).count)
     end
   end
-
-  describe 'Testing punctuation method' do
-    it 'Returns an error when are more brackets open' do
-      check_error(original)
-      expect(@punctation_arr.include?(sign: punct, result: 1, msg: 'to close')).to eql(true)
-    end
-  end
 end
